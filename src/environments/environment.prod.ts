621008374b78e3c40e1c0ac7cfa4ef27012e7772
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-03-07 17:41:29 -0400

    deploy version 2026

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   //local
-  apiUrl: "http://127.0.0.1:8000/api",
-  apiUrlMedia: "http://127.0.0.1:8000/storage/",
+  //apirest local
+  // apiUrl: "http://127.0.0.1:8000/api",
+  // apiUrlMedia: "http://127.0.0.1:8000/storage",
   
   //remoto
-  // apiUrl: "https://ursigalletti.net/backend-api/public/api",
-  // apiUrlMedia: "https://ursigalletti.net/backend-api/storage/app/",
+  apiUrl: "https://ursigalletti.net/backend-api-ursi2/public/api",
+  apiUrlMedia: "https://ursigalletti.net/backend-api-ursi2/storage/",
   clientIdSandbox: 'AVqPEgCGvjhPeugklbaUQUmujPZEVb53TZBYWUbjBUvppz4bOoXKgXot2sZtXQgH5AHsdSp-WYuB83nM',//ursi
   clientId: 'ATLlUShSXBpJNhgTm6M0H2l1yq42P9fYBmNGDYdKRAIAy8hMcf_wwnCUUIaUHev8LcrVKK_oUxUsYXSs'//ursi
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,13 @@
 
 export const environment = {
   production: false,
-  //apirest vps remoto
-  apiUrl: "http://127.0.0.1:8000/api",
-  apiUrlMedia: "http://127.0.0.1:8000/storage/",
+  //apirest local
+  // apiUrl: "http://127.0.0.1:8000/api",
+  // apiUrlMedia: "http://127.0.0.1:8000/storage",
+  
   //remoto
-  // apiUrl: "https://ursigalletti.net/backend-api/public/api",
-  // apiUrlMedia: "https://ursigalletti.net/backend-api/storage/",
+  apiUrl: "https://ursigalletti.net/backend-api-ursi2/public/api",
+  apiUrlMedia: "https://ursigalletti.net/backend-api-ursi2/storage/",
   // clientIdSandbox: 'AXlazeNsZ0CmjfJIronSzcqzw4hLHkcoVEM5fO5BY7AbD-_GhKoKezRcavq6-T4kQuRqaTXFB_VXmheG',//malcolm
   clientIdSandbox: 'AVqPEgCGvjhPeugklbaUQUmujPZEVb53TZBYWUbjBUvppz4bOoXKgXot2sZtXQgH5AHsdSp-WYuB83nM',//ursi
   clientId: 'ATLlUShSXBpJNhgTm6M0H2l1yq42P9fYBmNGDYdKRAIAy8hMcf_wwnCUUIaUHev8LcrVKK_oUxUsYXSs'//ursi
